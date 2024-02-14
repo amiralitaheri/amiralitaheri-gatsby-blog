bfc25343939b1bd1b3c7953b6704e563ac7581c6
Amirali Taheri <amiralitaheri64@gmail.com>
2024-02-14 13:36:05 +0000

    refactor: Simplify compileMDX

--- src/components/RenderMdx.tsx
+++ src/components/RenderMdx.tsx
@@ -2,16 +2,13 @@ import { FC } from "react";
 import { compileSync, runSync } from "@mdx-js/mdx";
 import * as runtime from "react/jsx-runtime";
 
-function compileMDX(mdx: string) {
-  const code = String(
+const compileMDX = (mdx: string) =>
+  String(
     compileSync(mdx, {
       outputFormat: "function-body",
       development: false,
     })
   );
-
-  return code;
-}
 
 const RenderMdx: FC<{ mdx: string }> = ({ mdx }) => {
   return runSync(compileMDX(mdx), runtime).default();
